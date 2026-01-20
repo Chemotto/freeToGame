d8bb89899bfc51fd57c3f7cb7224bbe8033ed940
tardes <jmo.gonzalez@gmail.com>
2026-01-20 20:50:54 +0100

    ajustes para mostrar las imágenes de los juegos

---
+++ freeToGame/ViewCells/GameViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GameViewCell.swift
+//  freeToGame
+//
+//  Created by Tardes on 20/1/26.
+//
+
+import UIKit
+
+class GameViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
