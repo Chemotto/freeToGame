51f7a9b56ef026571d84a8d27766f75ea6110ac3
tardes <jmo.gonzalez@gmail.com>
2026-01-21 17:51:20 +0100

    añadidas las llamadas de las apis, la estructura de game para el json, y el scrollview

--- freeToGame/Data/Game.swift
+++ freeToGame/Data/Game.swift
@@ -12,7 +12,48 @@ struct game: Codable {
     let id: Int
     let title: String
     let thumbnail: String
+    let shortDescription: String
+    let gameUrl: String
+    let genre: String
+    let platform: String
+    let publiser: String
+    let developer: String
+    let releaseDate: String
+    let profileUrl: String
+    let description: String?
+    let systemRequirements: systemRequirements
+    let Screenshots: [Screenshots]?
     
     
     
+    
+    enum     CodingKeys: String, CodingKey {
+        case shortDescription = "short_description"
+        case gameUrl = "game_url"
+        case releaseDate = "release_date"
+        case profileUrl = "profile_url"
+        case systemRequirements = "minimum_system_requirements"
+    
+        
+        case id, title, thumbnail,genre, platform, publiser, developer, description, Screenshots
+        
+
+        
+    }
 }
+
+struct systemRequirements: Codable {
+    let os: String
+    let processor: String
+    let memory: String
+    let graphics: String
+    let storage: String
+}
+
+struct Screenshots: Codable {
+    let image: String
+}
+
+
+
+
